873ce24cd94437c9234c416e62548cfe4776583d
Winnie A. Damayo <winnie@treonhrx.com>
2018-09-04 11:54:09 +0800

    Update CI_hook_log.php

--- CI_hook_log.php
+++ CI_hook_log.php
@@ -13,12 +13,10 @@ class CI_Hook_Log
         $this->CI->config->load("log");
 
         $this->dbLog = $this->CI->load->database('log', TRUE);
-
-        $this->createTableIfNotExist();
     }
 
     public function log_activity()
-    {   
+    {
 
         $user = $this->CI->session->userdata('username');
 
@@ -28,30 +26,30 @@ class CI_Hook_Log
         }
 
         $is_logged  = $this -> CI -> config -> item("db_log_activity_activity");
-        
+
         if (!$is_logged) {
             return;
         }
 
         $application = $this->CI->config->item('index_page');
-        
+
         $module      = getURI(0);
         $tab         = getURI(1);
         $action      = getURI(2) . ' '. getURI(3) . ' '. getURI(4) . ' '. getURI(5) . ' '. getURI(6);
-        
+
         $action      =( empty(getURI(2)) ) ? 'view' : $action;
-        
+
         $post_data   = $this->CI->input->post();
-        
+
         $post_data   = (empty($post_data) || !$post_data) ? null : json_encode($post_data);
-        
+
         $username    = $this->CI->session->userdata("username");
-        
+
         $user_agent  = $this->CI->session->userdata("user_agent");
-        
+
         $ip_address  = $this->CI->session->userdata("ip_address");
-        
-        
+
+
         $query       = $this->CI->db->query('SELECT module_code FROM module WHERE module_id = ?', [$module])->row();
 
         $data = [
@@ -68,7 +66,7 @@ class CI_Hook_Log
         if (empty($post_data) ) {
             unset($data['post_data']);
         }
-        
+
         $this->dbLog->insert('setup.activity_log', $data);
 
     }//end log_activity
@@ -104,70 +102,6 @@ class CI_Hook_Log
             $dbLog -> close();
         }
     }
-
-    private function createTableIfNotExist()
-    {
-        $sql =  "
-                CREATE SCHEMA IF NOT EXISTS setup;
-
-                CREATE TABLE IF NOT EXISTS setup.activity_log
-                (
-                    application character varying NOT NULL,
-                    module character varying NOT NULL,
-                    tab character varying NOT NULL,
-                    action character varying NULL,
-                    post_data character varying,
-                    username character varying(100) NOT NULL,
-                    time_stamp timestamp without time zone NOT NULL DEFAULT now(),
-                    user_agent character varying,
-                    ip_address character varying
-                )
-                WITH (
-                    OIDS=FALSE
-                );
-                ALTER TABLE IF EXISTS setup.activity_log
-                    OWNER TO postgres;
-
-                CREATE TABLE IF NOT EXISTS setup.log_query
-                (
-                    username character varying(100) NOT NULL,
-                    ip_address character varying,
-                    user_agent character varying,
-                    time_stamp timestamp without time zone NOT NULL DEFAULT now(),
-                    query character varying
-                )
-                WITH (
-                    OIDS=FALSE
-                );
-                ALTER TABLE IF EXISTS setup.log_query
-                    OWNER TO postgres;
-                ";
-
-        $this->executeQuery($sql, $this->dbLog->database);
-
-    }//end createTableIfNotExist
-
-    private function executeQuery($sql, $dbName = 'template1')
-    {
-
-        $con = pg_connect("
-            host=".$this->dbLog->hostname." 
-            port=".$this->dbLog->port." 
-            dbname=".$dbName." 
-            user=".$this->dbLog->username." 
-            password=".$this->dbLog->password
-        );
-
-        if(pg_connection_status($con) !== PGSQL_CONNECTION_OK) {
-            echo "Failed to connect to database.\n";
-            exit(1);
-        }
-            
-        pg_query($con, $sql);
-    
-        pg_close($con);
-
-    }//end executeQuery
 }
 
 /* End of file CI_hook_log.php */
